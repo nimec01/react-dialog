789aaab5b1f4676e41f91dfe7f3e00ed1a0b7428
Nick <24428341+Nick46000@users.noreply.github.com>
2022-10-01 14:14:34 +0200

    chore: setup jest test environment

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    // '@testing-library/react/cleanup-after-each',
+    '@testing-library/jest-dom/extend-expect',
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
